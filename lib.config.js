0d378099c72ce8e4cf5c2b4f9eae045777d3594a
zyx <3306818069@qq.com>
2023-08-10 15:23:45 +0800

    lib文件报错

--- lib.config.js
+++ lib.config.js
@@ -1,43 +1,109 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-export default defineConfig({
-  build: {
-    //打包文件目录
-    outDir: "es",
-    //压缩
-    //minify: false,
-    rollupOptions: {
-      //忽略打包vue文件
-      external: ["vue"],
-      input: ["./package/index.js"],
-      output: [
-        {
-          //打包格式
-          format: "es",
-          //打包后文件名
-          entryFileNames: "[name].mjs",
-          //让打包目录和我们目录对应
-          preserveModules: true,
-          exports: "named",
-          //配置打包根目录
-          dir: "./dist/hx-design/es",
-        },
-        {
-          //打包格式
-          format: "cjs",
-          //打包后文件名
-          entryFileNames: "[name].js",
-          //让打包目录和我们目录对应
-          preserveModules: true,
-          exports: "named",
-          //配置打包根目录
-          dir: "./dist/hx-design/lib",
-        },
-      ],
+const path = require("path")
+const fs = require("fs-extra")
+// 引入 vite 的 build 方法，进行编译构建
+const { defineConfig, build } = require("vite")
+const vue = require("@vitejs/plugin-vue")
+const vueJSX = require("@vitejs/plugin-vue-jsx")
+const version = require("./package.json").version
+
+// 基础配置
+const baseConfig = defineConfig({
+  publicDir: false,
+  plugins: [vue(), vueJSX()]
+})
+const rollupOptions = defineConfig({
+  // that shouldn't be bundled
+  external: ["vue"],
+  globals: {
+    vue: "Vue"
+  }
+})
+// 组件库全局入口
+const compontsDir = path.resolve(__dirname, "./package/index.js")
+// 输出目录
+const outputDir = path.resolve(__dirname, "./build")
+// 生成 package.json
+const createPackageJson = name => {
+  const fileStr = `{
+    "name": "${name ? name : "hx-design"}",
+    "version": "${version}",
+    "description": "Vue3组件库",
+    "main": "${name ? "index.umd.js" : "hx-design.umd.js"}",
+    "module":"${name ? "index.mjs" : "hx-design.mjs"}",
+    "repository": {
+      "type": "git",
+      "url": "git+https://github.com/GGXXMM/vue3-ui.git"
     },
-    lib: {
-      entry: "./package/index.js",
-    },
-  },
-  plugins: [vue()],
-});
+    "keywords": ["vue3", "组件库", "UI"],
+    "author": "zhangyaxi",
+    "license": "ISC"
+  }
+  `
+  // 单个组件 or 全量
+  const filePath = path.resolve(
+    outputDir,
+    name ? `${name}/package.json` : `package.json`
+  )
+
+  fs.outputFile(filePath, fileStr, "utf-8")
+}
+
+/** 单组件按需构建 */
+const buildSingle = async name => {
+  await build(
+    defineConfig({
+      ...baseConfig,
+      build: {
+        lib: {
+          entry: path.resolve(compontsDir, name),
+          name: "index",
+          fileName: "index",
+          formats: ["es", "umd"]
+        },
+        rollupOptions,
+        outDir: path.resolve(outputDir, name)
+      }
+    })
+  )
+
+  createPackageJson(name)
+}
+
+/** 全量构建 */
+const buildAll = async () => {
+  await build(
+    defineConfig({
+      ...baseConfig,
+      build: {
+        lib: {
+          entry: compontsDir,
+          name: "hx-design",
+          fileName: "hx-design",
+          formats: ["es", "umd"]
+        },
+        rollupOptions,
+        outDir: outputDir
+      }
+    })
+  )
+
+  createPackageJson()
+}
+
+const buildLib = async () => {
+  await buildAll()
+
+  // 按需打包
+  fs.readdirSync(compontsDir)
+    .filter(name => {
+      // 获取组件的目录
+      const componentDir = path.resolve(compontsDir, name)
+      const isDir = fs.lstatSync(componentDir).isDirectory()
+      return isDir && fs.readdirSync(componentDir).includes("index.js")
+    })
+    .forEach(async name => {
+      await buildSingle(name)
+    })
+}
+
+buildLib()
