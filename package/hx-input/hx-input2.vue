70b1440d402bb0949b562f85329fc7125a1d450e
zyx <3306818069@qq.com>
2023-08-11 16:05:01 +0800

    文件结构修改

---
+++ package/hx-input/hx-input2.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hxInput">
+    <InputItem
+      v-bind="attrs"
+    >
+      <template v-for="k in Object.keys(slots)" #[k] :key="k">
+        <slot v-if="k == 'abc'" :name="k">
+            <div>abc</div>
+        </slot>
+        <slot v-else :name="k"></slot>
+      </template>
+    </InputItem>
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+};
+</script>
+
+<script setup>
+import { InputItem } from "mand-mobile-next";
+import { useAttrs, useSlots, ref } from "vue";
+
+const attrs = useAttrs();
+const slots = useSlots();
+</script>
+
+<style scoped lang="scss">
+@import "./hx-input.scss";
+</style>
