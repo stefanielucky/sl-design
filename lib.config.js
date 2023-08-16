2985ad26d77ec52be0a530274dc4e438d0f495bd
zyx <3306818069@qq.com>
2023-08-16 10:40:12 +0800

    配置修改

--- lib.config.js
+++ lib.config.js
@@ -1,111 +1,54 @@
-// 配置参考 https://juejin.cn/post/7255514764754894907?searchId=20230810094951DBABB962670772C9051B
+import {
+  defineConfig
+} from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from 'path';
 
-const path = require("path")
-const fs = require("fs-extra")
-// 引入 vite 的 build 方法，进行编译构建
-const { defineConfig, build } = require("vite")
-const vue = require("@vitejs/plugin-vue")
-const vueJSX = require("@vitejs/plugin-vue-jsx")
-const version = require("./package.json").version
 
-// 基础配置
-const baseConfig = defineConfig({
-  publicDir: false,
-  plugins: [vue(), vueJSX()]
-})
-const rollupOptions = defineConfig({
-  // that shouldn't be bundled
-  external: ["vue"],
-  globals: {
-    vue: "Vue"
-  }
-})
-// 组件库全局入口
-const compontsDir = path.resolve(__dirname, "./package/index.js")
-// 输出目录
-const outputDir = path.resolve(__dirname, "./build")
-// 生成 package.json
-const createPackageJson = name => {
-  const fileStr = `{
-    "name": "${name ? name : "garfield-design"}",
-    "version": "${version}",
-    "description": "Vue3组件库",
-    "main": "${name ? "index.umd.js" : "garfield-design.umd.js"}",
-    "module":"${name ? "index.mjs" : "garfield-design.mjs"}",
-    "repository": {
-      "type": "git",
-      "url": "git+https://github.com/GGXXMM/vue3-ui.git"
+export default defineConfig({
+  build: {
+    //打包文件目录
+    outDir: "lib/garfield-design",
+    //压缩
+    //minify: false,
+    rollupOptions: {
+      //忽略打包vue文件
+      external: ["vue"],
+      input: ["./package/index.js"],
+      output: [{
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+          globals: {
+            vue: 'Vue'
+          }
+        },{
+          //打包格式
+          format: "es",
+          //打包后文件名
+          entryFileNames: "[name].mjs",
+          //让打包目录和我们目录对应
+          preserveModules: true,
+          exports: "named",
+          //配置打包根目录
+          dir: "lib/garfield-design/es",
+        },
+        {
+          //打包格式
+          format: "cjs",
+          //打包后文件名
+          entryFileNames: "[name].js",
+          //让打包目录和我们目录对应
+          preserveModules: true,
+          exports: "named",
+          //配置打包根目录
+          dir: "lib/garfield-design/lib",
+        },
+      ],
     },
-    "keywords": ["vue3", "组件库", "UI"],
-    "author": "zhangyaxi",
-    "license": "ISC"
-  }
-  `
-  // 单个组件 or 全量
-  const filePath = path.resolve(
-    outputDir,
-    name ? `${name}/package.json` : `package.json`
-  )
-
-  fs.outputFile(filePath, fileStr, "utf-8")
-}
-
-/** 单组件按需构建 */
-const buildSingle = async name => {
-  await build(
-    defineConfig({
-      ...baseConfig,
-      build: {
-        lib: {
-          entry: path.resolve(compontsDir, name),
-          name: "index",
-          fileName: "index",
-          formats: ["es", "umd"]
-        },
-        rollupOptions,
-        outDir: path.resolve(outputDir, name)
-      }
-    })
-  )
-
-  createPackageJson(name)
-}
-
-/** 全量构建 */
-const buildAll = async () => {
-  await build(
-    defineConfig({
-      ...baseConfig,
-      build: {
-        lib: {
-          entry: compontsDir,
-          name: "garfield-design",
-          fileName: "garfield-design",
-          formats: ["es", "umd"]
-        },
-        rollupOptions,
-        outDir: outputDir
-      }
-    })
-  )
-
-  createPackageJson()
-}
-
-const buildLib = async () => {
-  await buildAll()
-
-  // 按需打包
-  fs.readdirSync(compontsDir)
-    .filter(name => {
-      // 获取组件的目录
-      const componentDir = path.resolve(compontsDir, name)
-      const isDir = fs.lstatSync(componentDir).isDirectory()
-      return isDir && fs.readdirSync(componentDir).includes("index.js")
-    })
-    .forEach(async name => {
-      await buildSingle(name)
-    })
-}
-
-buildLib()
+    lib: {
+      entry: path.join(__dirname, './package/index.js'),
+      name: 'garfield-design',
+      fileName: 'garfield-design'
+    },
+  },
+  plugins: [vue()],
+});
